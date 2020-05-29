aa4f4a3d102baa6c08c50471a82ef853a112ed6d
pawel <pawellechtoja@gmail.com>
2020-05-29 23:10:00 +0200

    different output modes added

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from dijkstra.dijkstra import dijkstra
 
 
 def main():
-    if len(sys.argv) != 12 or sys.argv[1] == "help":
+    if len(sys.argv) != 13 or sys.argv[1] == "help":
         print(
             "USAGE: python3 main.py input_file source target number_of_roots ant_colony_size ant_iterations alpha "
-            "beta evaporation_speed min_pheromones ant_algo_type")
+            "beta evaporation_speed min_pheromones ant_algo_type full_path")
         exit(1)
     input_file = sys.argv[1]
     start_city = sys.argv[2]
@@ -22,14 +22,20 @@ def main():
     evaporation_speed = float(sys.argv[9])
     min_pheromones = float(sys.argv[10])
     ant_algo_type = sys.argv[11]
+    full_path = int(sys.argv[12])
 
     usa_map = USAMap(input_file)
     ant_colony_algorithm = AntColonyAlgorithm(ant_colony_size=ant_colony_size, iterations_nr=ants_iterations,
                                               alpha=alpha, beta=beta,
                                               evaporation_speed=evaporation_speed, min_pheromones_amount=min_pheromones)
     yen = YenAlgorithm(ant_colony_algorithm)
-    for n, path in enumerate(yen.run(number_of_roots, usa_map, start_city, stop_city, ant_algo_type)):
-        print(path.cost, end=',')
+
+    if full_path:
+        for n, path in enumerate(yen.run(number_of_roots, usa_map, start_city, stop_city, ant_algo_type)):
+            print(n+1, path, path.cost)
+    else:
+        for n, path in enumerate(yen.run(number_of_roots, usa_map, start_city, stop_city, ant_algo_type)):
+            print(path.cost, end=',')
 
 
 if __name__ == "__main__":
